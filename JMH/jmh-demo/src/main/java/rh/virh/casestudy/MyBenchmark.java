1bc2f4c0492a90bd7e8e05322ba97c6e95c7838f
virh <virgil.hong@aliyun.com>
2018-05-31 17:46:58 +0800

    add jmh test log

--- JMH/jmh-demo/src/main/java/rh/virh/casestudy/MyBenchmark.java
+++ JMH/jmh-demo/src/main/java/rh/virh/casestudy/MyBenchmark.java
@@ -26,12 +26,47 @@
 package rh.virh.casestudy;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MyBenchmark {
 
-    @Benchmark
-    public void testMethod() {
-        // place your benchmarked code here
-    }
-
+	  private static final Logger logger = LoggerFactory.getLogger(MyBenchmark.class);
+	  
+	  @Benchmark
+	  public void testConcatenatingStrings() {
+	 
+	    String x = "", y = "", z = "";
+	 
+	    for (int i = 0; i <100; i++) {
+	      x += i; y += i; z += i;
+	 
+	      logger.debug("Concatenating strings " + x + y + z);
+	    }
+	  }
+	  
+	  @Benchmark
+	  public void testVariableArguments() {
+	   
+	    String x = "", y = "", z = "";
+	   
+	    for (int i = 0; i < 100; i++) {
+	      x += i; y += i; z += i;
+	   
+	      logger.debug("Variable arguments {} {} {}", x, y, z);
+	    }
+	  }
+	  
+	  @Benchmark
+	  public void testIfDebugEnabled() {
+	   
+	    String x = "", y = "", z = "";
+	   
+	    for (int i = 0; i < 100; i++) {
+	      x += i; y += i; z += i;
+	   
+	      if (logger.isDebugEnabled())
+	        logger.debug("If debug enabled {} {} {}", x, y, z);
+	    }
+	  }
 }
