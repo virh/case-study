1c0c7b1f367f9ba3cdab640025a8cc88c37be663
virh <virgil.hong@aliyun.com>
2019-04-15 17:58:37 +0800

    add zipkin and sleuth demo

---
+++ springcloud/zipkin2/src/main/java/com/ranhfun/zipkin/ZipkinApplication2.java
@@ -0,0 +1,45 @@
+package com.ranhfun.zipkin;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ZipkinApplication2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication2.class, args);
+	}
+
+}
+
+class ZipkinController {
+
+    @Autowired
+    RestTemplate restTemplate;
+ 
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+ 
+    private static final Logger LOG = Logger.getLogger(ZipkinController.class.getName());
+     
+    @GetMapping(value="/zipkin2")
+    public String zipkinService1()
+    {
+        LOG.info("Inside zipkinService 2..");
+         
+         String response = (String) restTemplate.exchange("http://localhost:8082/zipkin3",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}).getBody();
+        return "Hi...";
+    }
+	
+}
