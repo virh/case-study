745acfbdceae4225f7f402e02e560249e443d465
virh <virgil.hong@aliyun.com>
2019-11-07 11:35:22 +0800

    add actuator prometheus support

--- springcloud/actuator-demo/src/main/java/com/ranhfun/actuator/actuatordemo/ActuatorSecurityConfig.java
+++ springcloud/actuator-demo/src/main/java/com/ranhfun/actuator/actuatordemo/ActuatorSecurityConfig.java
@@ -22,10 +22,12 @@ public class ActuatorSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().requestMatchers(EndpointRequest.to(ShutdownEndpoint.class)).hasRole("ACTUATOR_ADMIN")
-				.requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
-				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().antMatchers("/")
-				.permitAll().antMatchers("/**").authenticated().and().httpBasic();
+		http.authorizeRequests()
+			.requestMatchers(EndpointRequest.to(ShutdownEndpoint.class)).hasRole("ACTUATOR_ADMIN")
+			.requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+			.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+			.antMatchers("/").permitAll()
+			.antMatchers("/**").authenticated().and().httpBasic();
 	}
 
 }
