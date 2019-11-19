daa119383d887c8efdf0ebeb1cbc6cb2473d8a79
virh <virgil.hong@aliyun.com>
2019-11-19 10:22:16 +0800

    add spring boot mybatis demo

---
+++ springcloud/mybatis-demo/src/main/resources/create.sql
@@ -0,0 +1,9 @@
+create database demo;
+create table employees
+(
+   id integer not null,
+   first_name varchar(255) not null, 
+   last_name varchar(255) not null,
+   email_address varchar(255) not null,
+   primary key(id)
+);
